1e7dd33baf7287abc78ea274880093bd982ccbfa
Cengiz Okan <cengizokan@outlook.com>
2026-04-21 00:58:36 +0300

    add Filter and Compare executors

--- setup.py
+++ setup.py
@@ -1,27 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name="ImageProcessor",
+    name="DemoPackage",
     version="0.0.1",
-    author="DigiNova",
-    author_email='info@diginova.com.tr',
-    description="Image Filter and Compare Package",
-    url='https://github.com/CengizOkan/demo2',
-    license='MIT',
-    install_requires=['sdk', 'opencv-python-headless', 'numpy'],
-
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-
-    packages=[
-        'novavision.ImageProcessor',
-        'novavision.ImageProcessor.executors',
-        'novavision.ImageProcessor.models',
-        'novavision.ImageProcessor.utils',
-    ],
-    package_dir={'novavision.ImageProcessor': 'src'},
+    author="Cengiz",
+    description="Demo Package for NovaVision",
+    # 'sdk' yazan hatalı kısmı kaldırdık, sadece gerçekten gerekenleri bıraktık
+    install_requires=['opencv-python-headless', 'numpy'],
+    packages=setuptools.find_packages(),
     python_requires=">=3.6"
 )
