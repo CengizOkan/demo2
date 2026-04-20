963e2269ff56d917bf91351d5af7498563540365
Cengiz Okan <cengizokan@outlook.com>
2026-04-21 01:42:26 +0300

    add Filter and Compare executors

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ import setuptools
 setuptools.setup(
     name="DemoPackage",
     version="0.0.1",
-    author="Cengiz",
-    description="Demo Package for NovaVision",
-    # 'sdk' yazan hatalı kısmı kaldırdık, sadece gerçekten gerekenleri bıraktık
+    author="DigiNova",
+    author_email='info@diginova.com.tr',
+    description="Image Filter and Compare Package",
+    url='https://github.com/CengizOkan/demo2',
+    license='MIT',
     install_requires=['opencv-python-headless', 'numpy'],
-    packages=setuptools.find_packages(),
+    packages=[
+        'novavision.DemoPackage',
+        'novavision.DemoPackage.executors',
+        'novavision.DemoPackage.models',
+        'novavision.DemoPackage.utils',
+    ],
+    package_dir={'novavision.DemoPackage': 'src'},
     python_requires=">=3.6"
 )
