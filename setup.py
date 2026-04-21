5f80864bf9061637b9e5f842323b6c61e6a723b4
Cengiz Okan <cengizokan@outlook.com>
2026-04-21 16:50:03 +0300

    add Filter and Compare executors

--- setup.py
+++ setup.py
@@ -4,11 +4,18 @@ setuptools.setup(
     name="DemoPackage",
     version="0.1.0",
     author="Cengiz Okan",
-    packages=setuptools.find_namespace_packages(include=["components.*"]),
+    packages=[
+        "components",
+        "components.DemoPackage",
+        "components.DemoPackage.src",
+        "components.DemoPackage.src.models",
+        "components.DemoPackage.src.executors",
+        "components.DemoPackage.src.utils"
+    ],
     install_requires=[
         'numpy',
         'pydantic',
-        'opencv-python-headless' # Docker içinde hata vermemesi için headless sürüm
+        'opencv-python-headless'
     ],
     python_requires=">=3.8"
 )
