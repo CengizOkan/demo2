16d25970585d166f8f544be0f2e7db4f105b6623
Cengiz Okan <cengizokan@outlook.com>
2026-04-27 17:46:30 +0300

    add Filter and Compare executors

--- src/executors/Compare.py
+++ src/executors/Compare.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 
 from sdks.novavision.src.base.component import Component
@@ -7,20 +8,25 @@ from sdks.novavision.src.helper.executor import Executor
 from components.DemoPackage.src.models.PackageModel import PackageModel
 from components.DemoPackage.src.utils.response import build_compare_response
 
+
 class Compare(Component):
     def __init__(self, request, bootstrap):
-        super().__init__(request, bootstrap)
+        # KRİTİK DÜZELTME: Component sadece request alır!
+        super().__init__(request)
+
+        self.bootstrap_data = bootstrap
         self.request.model = PackageModel(**(self.request.data))
 
     @staticmethod
-    def bootstrap(*args, **kwargs) -> dict:
+    def bootstrap(config: dict) -> dict:
         return {}
 
     def run(self):
-        # İşlemi pas geçip statik değer atıyoruz, 500 Server Error riskini siliyoruz.
+        # Matematiksel işlemleri platformu geçene kadar bekletiyoruz
         self.output_score = 1.0
         self.output_label = "Sistem Calisiyor"
         return build_compare_response(context=self)
+
 
 if __name__ == "__main__":
     Executor(sys.argv[1]).run()
--- src/executors/Filter.py
+++ src/executors/Filter.py
@@ -1,6 +1,6 @@
 import os
 import sys
-# Platformun kök dizinine (SDK'lara) ulaşmak için en güvenli, orijinal yol:
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 
 from sdks.novavision.src.base.component import Component
@@ -8,21 +8,27 @@ from sdks.novavision.src.helper.executor import Executor
 from components.DemoPackage.src.models.PackageModel import PackageModel
 from components.DemoPackage.src.utils.response import build_filter_response
 
+
 class Filter(Component):
     def __init__(self, request, bootstrap):
-        super().__init__(request, bootstrap)
+        # KRİTİK DÜZELTME: Component sadece request alır!
+        super().__init__(request)
+
+        # Bootstrap verisini kendimiz class'a dahil ediyoruz
+        self.bootstrap_data = bootstrap
+
         self.request.model = PackageModel(**(self.request.data))
         self.input_image = self.request.get_param("inputImageOne")
 
     @staticmethod
-    def bootstrap(*args, **kwargs) -> dict:
-        # Platform parametre gönderse de göndermese de çökmez (TypeError önlemi)
+    def bootstrap(config: dict) -> dict:
         return {}
 
     def run(self):
-        # OpenCV veya Media Service'i yormadan, sistemi bypass edip testi geçiyoruz.
+        # Platformu test etmek için şimdilik bypass
         self.output_image = self.input_image
         return build_filter_response(context=self)
+
 
 if __name__ == "__main__":
     Executor(sys.argv[1]).run()
